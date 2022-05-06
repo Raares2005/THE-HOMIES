2c57e9365fc85b23d4143225122f4bdae9a159fd
Raares2005 <rares.croitoru8@gmail.com>
2022-05-07 00:36:25 +0300

    navbar

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector("#hamburger");
-const navbarNAV = document.querySelector(".navbar-nav");
-
-hamburger.addEventListener('click', () => {
-    navbarNAV.classList.toggle('show');
-});
